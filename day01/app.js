515d4bff26f44b130840be6ddf651cda7b60a0c8
Etienne Dldc <etienne@twik-labs.fr>
2015-10-29 10:26:35 +0100

    Init canvas

--- day01/app.js
+++ day01/app.js
@@ -1,5 +1,19 @@
-(function () {
+$(function () {
 
-  
+  var sketch = function( p ) {
 
-})()
+    var winHeight = $(window).height();
+    var winWidth = $(window).width();
+
+    p.setup = function setup() {
+      p.createCanvas(winWidth, winHeight);
+    };
+
+    p.draw = function draw() {
+      p.background(0);
+    };
+  };
+
+  var myp5 = new p5(sketch);
+
+})
