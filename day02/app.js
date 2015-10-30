3a0a36c2b8e9238331bc6d92d738a11ed90eed2d
Etienne Dldc <etienne@twik-labs.fr>
2015-10-30 11:48:00 +0100

    Day 3

--- day02/app.js
+++ day02/app.js
@@ -5,186 +5,115 @@ $(function () {
     var winHeight = $(window).height();
     var winWidth = $(window).width();
 
-    var song1, song2, fft, analyzer, gui, setts;
-    var globalRotate = 0;
-    var slow = 0;
-    var points = [];
+    var flatColors = [
+      { name: 'turquoise', color : '#1abc9c' },
+      { name: 'emerland', color : '#2ecc71' },
+      { name: 'peterriver', color : '#3498db' },
+      { name: 'amethyst', color : '#9b59b6' },
+      { name: 'wetasphalt', color : '#34495e' },
+      { name: 'greensea', color : '#16a085' },
+      { name: 'nephritis', color : '#27ae60' },
+      { name: 'belizehole', color : '#2980b9' },
+      { name: 'wisteria', color : '#8e44ad' },
+      { name: 'midnightblue', color : '#2c3e50' },
+      { name: 'sunflower', color : '#f1c40f' },
+      { name: 'carrot', color : '#e67e22' },
+      { name: 'alizarin', color : '#e74c3c' },
+      { name: 'clouds', color : '#ecf0f1' },
+      { name: 'concrete', color : '#95a5a6' },
+      { name: 'orange', color : '#f39c12' },
+      { name: 'pumpkin', color : '#d35400' },
+      { name: 'pomegranate', color : '#c0392b' },
+      { name: 'silver', color : '#bdc3c7' },
+      { name: 'asbestos', color : '#7f8c8d' },
+    ];
+
+    var pixGrid = [];
+    var mX = 0;
+    var mY = 0;
 
     var Settings = function() {
-      this.maxPointSize = 30;
-      this.pointPaddingX = 30;
-      this.pointPaddingY = -5;
-      this.initDistance = 4;
-      this.speed = 50;
-      this.amplifier = 4;
-      this.maxPoints = 200;
-      this.rotation = true;
-      this.rotationInvert = true;
-      this.rotationSpeed = 120;
-      this.pointStrokeLimitMultiplier = 0.15;
-      this.playPause = function playPause() {
-        if ( song.isPlaying() ) { // .isPlaying() returns a boolean
-          song.pause();
-          p.noLoop();
-        } else {
-          song.play();
-          p.loop();
-        }
-      }
-      this.update();
+      this.followMouse = true;
+      this.probaLeft = 0.5;
+      this.probaRight = 0.5;
+      this.probaTop = 0.5;
+      this.probaBottom = 0.5;
     };
     Settings.prototype.update = function () {
-      this.maxPointSizeWithPaddingX = this.maxPointSize + (2*this.pointPaddingX);
-      this.maxPointSizeWithPaddingY = this.maxPointSize + (2*this.pointPaddingY);
-      this.pointStrokeLimit = this.maxPointSize * this.pointStrokeLimitMultiplier;
     };
-
-    p.preload = function() {
-      song1 = p.loadSound('http://codepen.twik-labs.fr/codevember-2015/day-01/music-01.mp3');
-      song2 = p.loadSound('http://codepen.twik-labs.fr/codevember-2015/day-01/hello.mp3');
-    }
 
     p.setup = function setup() {
-      var song  = song2;
-
-      setts = new Settings();
-      // dat-gui
-      gui = new dat.GUI();
-      gui.add(setts, 'playPause');
-      gui.add(setts, 'maxPointSize', 1, 40);
-      gui.add(setts, 'amplifier', 0.5, 30);
-      gui.add(setts, 'pointPaddingX', -10, 30);
-      gui.add(setts, 'pointPaddingY', -10, 30);
-      gui.add(setts, 'initDistance', 1, 300);
-      gui.add(setts, 'maxPoints', 200, 3000);
-      gui.add(setts, 'speed', 1, 100);
-      gui.add(setts, 'rotationSpeed', 1, 400);
-      gui.add(setts, 'pointStrokeLimitMultiplier', 0.01, 1);
-      gui.add(setts, 'rotation');
-      gui.add(setts, 'rotationInvert');
+      // Init canvas
       p.createCanvas(winWidth, winHeight);
-      p.push();
-      p.translate(winWidth/2, winHeight/2);
-      song.play();
+      // dat-gui
+      setts = new Settings();
+      gui = new dat.GUI();
+      gui.add(setts, 'followMouse');
+      var f1 = gui.addFolder('No follow mode');
+      f1.add(setts, 'probaLeft', 0, 1);
+      f1.add(setts, 'probaRight', 0, 1);
+      f1.add(setts, 'probaTop', 0, 1);
+      f1.add(setts, 'probaBottom', 0, 1);
+
+      console.log();
+
       p.background(getFlatColor('wetasphalt'));
-      fft = new p5.FFT();
-      fft.setInput(song);
-      analyzer = new p5.Amplitude();
-      analyzer.setInput(song);
+
+      for (var i = 0; i < p.width; i++) {
+        pixGrid[i] = [];
+      }
+
+      // click
+      $(p.canvas).click(function(event) {
+        var color = getFlatColor('clouds');
+        drawPiwel(p.mouseX, p.mouseY, color);
+      });
+
+      p.noLoop();
     };
 
-    p.draw = function() {
-      slow = slow + (setts.speed / 100);
-      if (slow >= 1) {
-        slow = slow % 1;
-        p.background(getFlatColor('midnightblue'));
-        var spectrum = fft.analyze();
-        spectrum = spectrum.slice(100, 800);
-        var amplitude = analyzer.getLevel();
-        addPoint(amplitude, spectrum);
-        setts.update();
-        drawSpiral();
-      }
+    p.mouseMoved = function () {
+      mX = p.mouseX;
+      mY = p.mouseY;
     }
 
-    function drawSpiral() {
-      // Find final rotate angle
-      p.push();
-        if (setts.rotation) {
-          if (setts.rotationInvert) {
-            globalRotate -= (setts.rotationSpeed / 2000);
-          } else {
-            globalRotate += (setts.rotationSpeed / 2000);
-          }
-          p.rotate(globalRotate);
+    function drawPiwel(x, y) {
+      if (x < p.width && x > 0 && y < p.height && y > 0) {
+        if (pixGrid[x][y] !== 1) {
+          p.noStroke();
+          p.fill(getFlatColor('clouds'));
+          pixGrid[x][y] = 1;
+          p.rect(x, y, 1, 1);
+
+          (function(x, y) {
+            window.setTimeout(function () {
+
+              if (setts.followMouse) {
+                var leftForce = (mX - x) / (p.width); // -1 -> 1
+                var topForce = (mY - y) / (p.height); // -1 -> 1
+                leftForce = p.map(leftForce, -1, 1, 0, 1);
+                topForce = p.map(topForce, -1, 1, 0, 1);
+                var leftProb = leftForce;
+                var rightProb = 1 - leftForce;
+                var topProb = topForce;
+                var bottomProb = 1 - topForce;
+              } else {
+                var leftProb = 1 - setts.probaLeft;
+                var rightProb = 1 - setts.probaRight;
+                var topProb = 1 - setts.probaTop;
+                var bottomProb = 1 - setts.probaBottom;
+              }
+              if (p.random() > leftProb ) { drawPiwel(x-1, y) } // Left
+              if (p.random() > rightProb ) { drawPiwel(x+1, y) } // Right
+              if (p.random() > topProb ) { drawPiwel(x, y-1) } // Top
+              if (p.random() > bottomProb ) { drawPiwel(x, y+1) } // Bottom
+            }, 0);
+          })(x, y);
         }
-        angle = 0;
-        for (var i = 0; i < points.length; i++) {
-          var nbrOrTurn = angle/(2*p.PI);
-          var distance = setts.initDistance + (nbrOrTurn * setts.maxPointSizeWithPaddingX);
-          var circonference = (distance*2) * p.PI;
-          var nbrOfCircle = circonference / setts.maxPointSizeWithPaddingY;
-          var incrementAngle = (2*p.PI) / nbrOfCircle;
-          angle = angle + incrementAngle;
-
-          var x = p.sin(angle)*distance;
-          var y = p.cos(angle)*distance;
-          displayPoints(points[i], x, y);
-        }
-      p.pop();
-    }
-
-    function displayPoints(data, x, y) {
-      var size = data.size * setts.amplifier;
-      if (data.size > setts.pointStrokeLimit ) {
-        p.noFill();
-        p.stroke(data.color);
-        p.strokeWeight(p.map(setts.pointStrokeLimit, setts.maxPointSize, 1, setts.maxPointSize/2));
-      } else {
-        p.fill(data.color);
-        p.noStroke();
-      }
-      p.ellipse(x,y, size, size);
-    }
-
-    function getMaxFreqGroup(data, nbrOfGroups) {
-      var groupedFreq = groupFreq(data, nbrOfGroups);
-      return groupedFreq.indexOf(_.max(groupedFreq));
-    }
-
-    function groupFreq(data, nbrOfGroups) {
-      var result = [];
-      var size = p.floor(data.length / nbrOfGroups);
-      for (var i = 0; i < (data.length - size ) ; i += size) {
-        var start = i;
-        var end = start + size;
-        var sum = 0;
-        for (var j = start; j < end; j++) {
-          sum += data[j];
-        }
-        result.push(p.floor(sum/size));
-      }
-      return result;
-    }
-
-    function addPoint(amplitude, spectrum) {
-      var maxFreqGroup = getMaxFreqGroup(spectrum, 100);
-      var colors = getFlatColorArray(['turquoise','emerland', 'peterriver', 'amethyst', 'greensea', 'nephritis', 'belizehole', 'wisteria', 'sunflower', 'carrot', 'alizarin', 'clouds', 'concrete', 'orange', 'pumpkin', 'pomegranate', 'silver', 'asbestos']);
-      var colorIndex = p.floor(maxFreqGroup % colors.length);
-      var color = colors[colorIndex];
-      var size = p.map(amplitude, 0, 1, 0, setts.maxPointSize);
-      points.push({
-        size: size,
-        color: color
-      });
-      if (points.length > setts.maxPoints) {
-        points = points.slice(-setts.maxPoints);
       }
     }
 
     function getFlatColor(name) {
-      var flatColors = [
-        { name: 'turquoise', color : '#1abc9c' },
-        { name: 'emerland', color : '#2ecc71' },
-        { name: 'peterriver', color : '#3498db' },
-        { name: 'amethyst', color : '#9b59b6' },
-        { name: 'wetasphalt', color : '#34495e' },
-        { name: 'greensea', color : '#16a085' },
-        { name: 'nephritis', color : '#27ae60' },
-        { name: 'belizehole', color : '#2980b9' },
-        { name: 'wisteria', color : '#8e44ad' },
-        { name: 'midnightblue', color : '#2c3e50' },
-        { name: 'sunflower', color : '#f1c40f' },
-        { name: 'carrot', color : '#e67e22' },
-        { name: 'alizarin', color : '#e74c3c' },
-        { name: 'clouds', color : '#ecf0f1' },
-        { name: 'concrete', color : '#95a5a6' },
-        { name: 'orange', color : '#f39c12' },
-        { name: 'pumpkin', color : '#d35400' },
-        { name: 'pomegranate', color : '#c0392b' },
-        { name: 'silver', color : '#bdc3c7' },
-        { name: 'asbestos', color : '#7f8c8d' },
-      ];
       var color = _.find(flatColors, {name: name}, 'color');
       if (color == undefined) {
         throw new Error('Can\'t find color "' + name + '"');
@@ -199,6 +128,11 @@ $(function () {
         result.push(getFlatColor(list[i]));
       }
       return result;
+    }
+
+    function getRandomFlatColor() {
+      var index = p.floor(p.random(flatColors.length));
+      return flatColors[index].color;
     }
 
   };
---
+++ day03/app.js
@@ -0,0 +1,149 @@
+$(function () {
+
+  var winHeight = $(window).height();
+  var winWidth = $(window).width();
+
+  var flatColors = [
+    { name: 'turquoise', color : '#1abc9c' },
+    { name: 'emerland', color : '#2ecc71' },
+    { name: 'peterriver', color : '#3498db' },
+    { name: 'amethyst', color : '#9b59b6' },
+    { name: 'wetasphalt', color : '#34495e' },
+    { name: 'greensea', color : '#16a085' },
+    { name: 'nephritis', color : '#27ae60' },
+    { name: 'belizehole', color : '#2980b9' },
+    { name: 'wisteria', color : '#8e44ad' },
+    { name: 'midnightblue', color : '#2c3e50' },
+    { name: 'sunflower', color : '#f1c40f' },
+    { name: 'carrot', color : '#e67e22' },
+    { name: 'alizarin', color : '#e74c3c' },
+    { name: 'clouds', color : '#ecf0f1' },
+    { name: 'concrete', color : '#95a5a6' },
+    { name: 'orange', color : '#f39c12' },
+    { name: 'pumpkin', color : '#d35400' },
+    { name: 'pomegranate', color : '#c0392b' },
+    { name: 'silver', color : '#bdc3c7' },
+    { name: 'asbestos', color : '#7f8c8d' },
+  ];
+
+  function getFlatColor(name) {
+    var color = _.find(flatColors, {name: name}, 'color');
+    if (color == undefined) {
+      throw new Error('Can\'t find color "' + name + '"');
+      return;
+    }
+    return color.color;
+  }
+
+  function getFlatColorArray(list) {
+    var result = [];
+    for (var i = 0; i < list.length; i++) {
+      result.push(getFlatColor(list[i]));
+    }
+    return result;
+  }
+
+  function getRandomFlatColor() {
+    var index = p.floor(p.random(flatColors.length));
+    return flatColors[index].color;
+  }
+
+  var Settings = function() {
+    this.mode = 'video';
+    this.size = 800;
+  };
+  Settings.prototype.update = function () {
+  };
+
+  var main = false;
+  var work = false;
+  var captureFlow;
+  var capture = false;
+  var ratio = false;
+  var pixModelImage;
+  var result;
+
+  var workP5 = new p5(workCanvas);
+  var mainP5 = new p5(mainCanvas);
+
+  function mainCanvas ( p ) {
+    p.setup = function setup() {
+      // Init canvas
+      main = p;
+      p.createCanvas(winWidth, winHeight);
+      p.translate(winWidth/2, winHeight/2);
+      p.scale(-1, 1);
+      // dat-gui
+      setts = new Settings();
+      gui = new dat.GUI();
+      gui.add(setts, 'mode', ['video', 'recursiveVideo'])
+      gui.add(setts, 'size', 200, 1000)
+
+      captureFlow = p.createCapture(p.VIDEO);
+      captureFlow.hide();
+
+    };
+
+    p.draw = function () {
+      p.background(getFlatColor('wetasphalt'));
+
+      if (captureFlow.time() > 0) {
+        if (ratio === false) {
+          ratio = captureFlow.width / captureFlow.height;
+        }
+        console.log(ratio);
+        capture = captureFlow.get();
+        if (setts.mode === 'video') {
+          p.image(capture, -setts.size/2, -(setts.size/ratio)/2, setts.size, setts.size/ratio );
+        } else
+        if (setts.mode === 'recursiveVideo') {
+          createPixModel(20, 20/ratio);
+          result = generateRecursive(capture, setts.size, setts.size/ratio);
+          p.image(result, -result.width/2, -result.height/2);
+        }
+      }
+
+    }
+
+    function createPixModel(width, height) {
+      work.image(capture, 0, 0, width, height);
+      pixModelImage = work.get(0, 0, width, height);
+    }
+
+    function generateRecursive(source, width, height) {
+      var pixelWidth = width / pixModelImage.width;
+      var pixelHeight = height / pixModelImage.height;
+      // Create pixelImageResized
+      work.image(source, 0, 0, pixelWidth, pixelHeight);
+      var pixelImageResized = work.get(0, 0, pixelWidth, pixelHeight);
+
+      work.resizeCanvas(width, height);
+      for (var i = 0; i < pixModelImage.width; i++) {
+        for (var j = 0; j < pixModelImage.height; j++) {
+          var left = i * pixelWidth;
+          var top = j * pixelHeight;
+          var colorPix = pixModelImage.get(i, j);
+          var color = work.color(colorPix[0], colorPix[1], colorPix[2] );
+          work.fill(color);
+          work.noTint();
+          work.rect(left, top, pixelWidth, pixelHeight);
+          work.tint(255, 128);
+          work.image(pixelImageResized, left, top, pixelWidth, pixelHeight);
+        }
+      }
+      return work.get(0, 0, width, height);
+    }
+  };
+
+  function workCanvas ( p ) {
+    p.setup = function setup() {
+      // Init canvas
+      p.createCanvas(winWidth, winHeight);
+      work = p;
+      $(p.canvas).css('display', 'none');
+      p.noLoop();
+      p.noStroke();
+    };
+  };
+
+})
